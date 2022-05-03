18abb4a749de412b32444652074180c6d33d4d82
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-05-03 04:04:07 -0300

    Configuracion de mysql en heroku.

--- src/database.js
+++ src/database.js
@@ -2,10 +2,11 @@ const mysql = require('mysql');
 
 // MySql
 const connection = mysql.createConnection({
-    host: process.env.HOST || 'localhost',
-    user: process.env.USER || 'root',
-    password: process.env.PASSWORD || '',
-    database: process.env.DATABASE || 'webSeyo'
+    host: 'us-cdbr-east-05.cleardb.net', // process.env.HOST || 'localhost',
+    user: 'b82f601af3f4db', // process.env.USER || 'root',
+    password: '2f6c1900', // process.env.PASSWORD || '',
+    database: 'heroku_3d8b9dff9129423', // process.env.DATABASE || 'webSeyo'
+    // mysql://b82f601af3f4db:2f6c1900@us-cdbr-east-05.cleardb.net/heroku_3d8b9dff9129423?reconnect=true
   });
 
 // Check connect
