e8323f7dfa5bbc148de44b1b557b7f45cc3d4580
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-05-03 04:42:47 -0300

    Correccion package.json main, etc

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     password: '2f6c1900', // process.env.PASSWORD || '',
     database: 'heroku_3d8b9dff9129423', // process.env.DATABASE || 'webSeyo'
     // mysql://b82f601af3f4db:2f6c1900@us-cdbr-east-05.cleardb.net/heroku_3d8b9dff9129423?reconnect=true
-  });
+});
 
 // Check connect
 connection.connect(error => {
