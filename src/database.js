586df01a6d93c26c400a04dc2bcdf0c77e8e0515
Adrianibarra97 <adrian__ibarra@hotmail.com>
2022-05-17 16:44:35 -0300

    Regreso a BBDD local para correciones en frontEnd.

--- src/database.js
+++ src/database.js
@@ -2,11 +2,10 @@ const mysql = require('mysql');
 
 // MySql
 const connection = mysql.createConnection({
-    host: 'us-cdbr-east-05.cleardb.net', // process.env.HOST || 'localhost',
-    user: 'b82f601af3f4db', // process.env.USER || 'root',
-    password: '2f6c1900', // process.env.PASSWORD || '',
-    database: 'heroku_3d8b9dff9129423', // process.env.DATABASE || 'webSeyo'
-    // mysql://b82f601af3f4db:2f6c1900@us-cdbr-east-05.cleardb.net/heroku_3d8b9dff9129423?reconnect=true
+    host: process.env.HOST || 'localhost',
+    user: process.env.USER || 'root',
+    password: process.env.PASSWORD || '',
+    database: process.env.DATABASE || 'webSeyo'
 });
 
 // Check connect
